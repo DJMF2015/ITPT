0d9c7de79a6b50384d6c78a517df111d02f68624
DJMF2015 <djm.fulton@gmail.com>
2020-04-22 15:53:14 +0100

    dice challenge completed. to add button handler

--- week10/DOM Challenge/index.js
+++ week10/DOM Challenge/index.js
@@ -1,5 +1,5 @@
-// alert(loaded)
-document.querySelectorAll('.list')[2].textContent = 'David Fulton';
-document.querySelectorAll('.list')[0].style.background = 'lightgrey';
-
-// document.querySelector('.list')[1].innerHTML = 'David ';
+// // alert(loaded)
+// document.querySelectorAll('.list')[2].textContent = 'David Fulton';
+// document.querySelectorAll('.list')[0].style.background = 'lightgrey';
+//
+// // document.querySelector('.list')[1].innerHTML = 'David ';
---
+++ week10/Dicee/index.js
@@ -0,0 +1,26 @@
+
+(function run(){
+
+  var randomNumber1 = (Math.floor(Math.random() * 6) + 1);
+  var imgPath = "images/dice" + randomNumber1 + ".png"
+  var img = document.querySelector(".img1");
+  img.setAttribute("src", imgPath);
+
+  var randomNumber2 = (Math.floor(Math.random() * 6) + 1);
+  var imgPath2 = "images/dice" + randomNumber2 + ".png";
+  var image2 = document.querySelector(".img2");
+  image2.setAttribute("src", imgPath2);
+
+
+  if (randomNumber1 > randomNumber2){
+    document.getElementsByTagName('h1')[0].innerHTML = "player1 is winner"
+  } else if (randomNumber1 < randomNumber2) {
+    document.getElementsByTagName('h1')[0].innerHTML = "player2 is winner"
+  } else if
+  (randomNumber1 === randomNumber2){
+    document.getElementsByTagName('h1')[0].innerHTML = "Player 1 & 2 Draw"
+  }
+
+})();
+
+// TO DO - Add a button event handler
