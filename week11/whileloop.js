066d71a14758b96e6894151295d857f630779842
DJMF2015 <djm.fulton@gmail.com>
2020-04-29 13:24:35 +0100

    tidy up to bottles on the wall

--- week11/whileloop.js
+++ week11/whileloop.js
@@ -7,24 +7,42 @@
 // }
 // document.getElementById("heading").innerHTML = text;
 
-// 99 bottles of beer on the wall, 99 bottles of beer.
-// Take one down and pass it around, 98 bottles of beer on the wall.
-
 let bottles = 99;
 var text = "";
 
 while(bottles>=1){
   let drink = bottles -1; //drink a bottle
   text += bottles + " bottles of beer on the wall," + bottles + "bottles of beer." +
-  "<br>" + "Take one down and pass it around, " + drink + "bottles of beer on the wall."
-  if (bottles === 2){
-    text += bottles + " bottle of beer on the wall," + bottles + "bottle of beer."
-    +
-    "<br>" + "Take one down and pass it around, no more bottles of beer on the wall."
+  "<br>" + "Take one down and pass it around, " + drink + "bottles of beer on the wall." + "<br><br>";
+  if (bottles === 1){
+    text += bottles + "<br>" +" bottle of beer on the wall," + "bottle of beer."
+    + "Take one down and pass it around, no more bottles of beer on the wall. " + "<br><br>"+
+    "Go to the store and buy some more, 99 bottles of beer on the wall."
   }
   bottles--;
 }
 document.getElementById('heading').innerHTML = text;
 
-// No more bottles of beer on the wall, no more bottles of beer.
-// Go to the store and buy some more, 99 bottles of beer on the wall.
+// var i= 99;
+//
+// var text = "";
+//
+// while(i > 0){
+//   var x = i - 1;
+//   if (i!=1)
+//   {
+//     text += i+" bottles of beer on the wall, "+i+" bottles of beer.<br>Take one down and pass it around, "+x+ " bottles of beer on the wall.<br><br>";
+//
+//   }
+//   else
+//
+//   {
+//     text +="<br>No more bottles of beer on the wall, no more bottles of beer.<br>Go to the store and buy some more, 99 bottles of beer on the wall. <br>";
+//
+//   }
+//
+//   i--;
+//
+// }
+//
+// document.getElementById("heading").innerHTML = text;
