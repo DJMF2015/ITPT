858aebd160187819235378f74636ab5a2868af2d
DJMF2015 <djm.fulton@gmail.com>
2020-05-13 15:43:13 +0100

    completion of bmi calculator

--- week13/app.js
+++ week13/app.js
@@ -11,10 +11,31 @@ router.get('/', function (req, res) {
 })
 
 router.post('/', function(req,res){
-  const input1  = req.body.input1;
-  const input2  = req.body.input2;
-  const result = Number(input1) + Number(input2)
-  res.send("<h1>The data was "+ result);
+  var weight  = req.body.weight;
+  var height  = req.body.height;
+  var result = "";
+
+  // var options = req.body.op;
+  // var result = "" ;
+  // switch (options) {
+  //   case '+':
+  //   result = Number(input1) + Number(input2)
+  //   break;
+  //   case '-':
+  //   result = Number(input1) - Number(input2)
+  //   break;
+  //   case '*':
+  //   result = Number(input1) * Number(input2)
+  //   case '/':
+  //   result = Number(input1) / Number(input2)
+  //   break;
+  //   default: console.log('please enter a number and operator')
+  // }
+
+  //Formula: weight (kg) / [height (m)]2
+
+  result = Number(weight) /  Number(height * height)
+  res.send("<h1>Result is "+ result);
 })
 
 router.get('/about', function (req, res) {
