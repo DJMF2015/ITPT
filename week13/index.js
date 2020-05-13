b41c76d5b34bd3f0dcea0abc8480946dac793a4b
DJMF2015 <djm.fulton@gmail.com>
2020-05-13 11:45:12 +0100

    adding boiler plate and dependencies for app

---
+++ week13/index.js
@@ -0,0 +1,8 @@
+const firstName= "dave"
+const secondName = "fulton"
+
+function data(firstName, secondName){
+  return firstName.charAt().toUpperCase()   + " " + secondName.slice(0);
+}
+
+console.log(data(firstName, secondName))
