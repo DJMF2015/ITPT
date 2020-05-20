36e71be44f2d9873e46e790d7b25a5d8f358150f
DJMF2015 <djm.fulton@gmail.com>
2020-05-20 11:35:59 +0100

    completion of exercise 1 superheroes api

---
+++ week14/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+const parser = require('body-parser');
+const heroes = require('superheroes');
+const villains =  require('supervillains');
+
+app.use(parser.urlencoded({extended:true}));
+app.use(express.static(__dirname+ '/'));
+
+//http:localhost:3000
+app.get('/', function(req,res){
+  res.sendFile(__dirname + '/source/index.html');
+});
+
+app.get('/heroes', function(req, res){
+  res.send("<h1>" + heroes.random());
+});
+
+// app.get('/dognames', function(req, res){
+//   res.send("<h1>" + heroes.random());
+// });
+
+app.get('/villains', function(req, res){
+  res.send("<h1>" + villains.random());
+})
+app.post('/', function(req, res){
+  var fullname = req.body.personname;
+  res.write("<h1> Welcome  " + fullname);
+  res.write("<br> <h1> your saviour is " + heroes.random());
+  res.write("<br><br> Please stay away from " + villains.random());
+  res.send();
+});
+
+app.get('/about', function(req, res){
+  res.sendFile(__dirname + "/source/about.html")
+});
+
+
+app.listen(port, function(req, res){
+  console.log("server is up and running on port" + port);
+});
