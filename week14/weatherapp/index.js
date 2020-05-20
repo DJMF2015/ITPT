958b8f2b3b9db343ef44794abb8d0a73d8e59f2e
DJMF2015 <djm.fulton@gmail.com>
2020-05-20 16:01:46 +0100

    completion of express weather app

--- week14/weatherapp/index.js
+++ week14/weatherapp/index.js
@@ -7,6 +7,7 @@ const villains =  require('supervillains');
 const dogNames = require('dog-names');
 const https = require('https');
 
+
 app.use(parser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/'));
 
@@ -15,9 +16,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + "/source/index.html");
 });
 
-app.get('/about', function(req, res){
-  res.sendFile(__dirname + "/source/about.html")
-});
 
 app.get('/heroes', function(req, res){
   res.send("<h1>" + heroes.random());
@@ -25,56 +23,57 @@ app.get('/heroes', function(req, res){
 app.get('/dognames', function(req, res){
   res.sendFile(__dirname + "/source/dogname.html")
 })
-
 app.get('/weather', function(req, res){
-  res.sendFile(__dirname + "/source/weather.html")
+  res.sendFile(__dirname + "/source/weather.html");
 });
 
 app.post('/weather', function(req, res){
-  const apikey = 'ed6e1836e5d9df68dd15510dbf78c494';
+
+  const apikey = "eab8f68e350ecc5328039365fa2c81f7";
   const city = req.body.wname;
   const unit = "metric"
   const country = req.body.country;
 
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&appid="+apikey + "&units="+unit;
+ const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&appid="+apikey + "&units="+unit;
+
+
   https.get(url, function(response){
     console.log(response.statusCode);
-    response.on("data", function(data){
-      var weatherData = JSON.parse(data);
-      //these are retrieved variable //
 
-      const name = weatherData.name;
-      const temp =  Math.round(weatherData.main.temp);
-      const Humidity = weatherData.main.humidity
-      const desc = weatherData.main.pressure;
-      const sunset = Math.round(weatherData.sys.sunset);
+      response.on("data", function(data){
+        var weatherData = JSON.parse(data);
 
+        //these are retrieved variable //
 
-      res.write("<h1> Country" + country + "city name: "+ name + "<br>")
-      res.write("<h1> Current temperature is: " + temp )
-      res.write("<br><br> The Humidity is: " + Humidity);
-      res.write("<br><br> The pressure is: " + desc);
-      res.write("<br><br> Sunset is at: " + sunset);
-      res.send();
-    }).on("error", (e) =>{
-      console.log(e);
-    })
-  })
+        var name = weatherData.name;
+        var temperature = Math.round(weatherData.main.temp) + " C";
+        var Humidity = weatherData.main.humidity;
+        var desc = weatherData.main.pressure;
+
+        res.write("<h1> Country: " +country + "  CityName : " + name + "<br>");
+        res.write("<h1> The Current temperature is : " + temperature);
+        res.write("<br><br> The Humidity is :  " + Humidity )
+        res.write("<br><br> The pressure is : " + desc);
+
+        res.send();
+      }).on('error', (e) => {
+  console.log(e);
+});
+  });
 })
 
 app.post('/names', function(req, res){
   var option = req.body.options;
 
   if(option == "male"){
-    res.send("<h1> The male dog name is : "+ dogNames.maleRandom());
+      res.send("<h1> The male dog name is : "+ dogNames.maleRandom());
   }
   else{
     res.send("<h1> The female dog name is :"+ dogNames.femaleRandom());
   }
-
-
 })
+
 
 app.get('/villains', function(req, res){
   res.send("<h1>" + villains.random());
@@ -85,6 +84,10 @@ app.post('/', function(req, res){
   res.write("<br> <h1> your saviour is " + heroes.random());
   res.write("<br><br> Please stay away from " + villains.random());
   res.send();
+});
+
+app.get('/about', function(req, res){
+  res.sendFile(__dirname + "/source/about.html")
 });
 
 
