f903a7bfa8b10e5cb40dd75ec1d1f5cd5accb07d
DJMF2015 <djm.fulton@gmail.com>
2020-05-29 09:33:09 +0100

    update and commit example1 and 2

---
+++ week14/example2/index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+var parser = require('body-parser');
+const heroes = require('superheroes');
+const villains =  require('supervillains');
+const dogNames = require('dog-names');
+const https = require('https');
+
+app.use(parser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/'));
+
+// http://localhost:3000 /
+app.get('/', function(req, res){
+  res.sendFile(__dirname + "/source/index.html");
+});
+
+app.get('/about', function(req, res){
+  res.sendFile(__dirname + "/source/about.html")
+});
+
+app.get('/heroes', function(req, res){
+  res.send("<h1>" + heroes.random());
+});
+app.get('/dognames', function(req, res){
+  res.sendFile(__dirname + "/source/dogname.html")
+})
+
+app.get('/weather', function(req, res){
+  res.sendFile(__dirname + "/source/weather.html")
+});
+
+app.post('/weather', function(req, res){
+  const apikey = 'ed6e1836e5d9df68dd15510dbf78c494';
+  const city = req.body.wname;
+  const unit = "metric"
+  const country = req.body.country;
+
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&appid="+apikey + "&units="+unit;
+  https.get(url, function(response){
+    // console.log(response.statusCode);
+    response.on("data", function(data){
+      var weatherData = JSON.parse(data);
+      //these are retrieved variable //
+
+      const name = weatherData.name;
+      const temp =  Math.round(weatherData.main.temp);
+      const Humidity = weatherData.main.humidity
+      const desc = weatherData.main.pressure;
+      const sunset = Math.round(weatherData.sys.sunset);
+
+
+      res.write("<h1> Country" + country + "city name: "+ name + "<br>")
+      res.write("<h1> Current temperature is: " + temp )
+      res.write("<br><br> The Humidity is: " + Humidity);
+      res.write("<br><br> The pressure is: " + desc);
+      res.write("<br><br> Sunset is at: " + sunset);
+      res.send();
+    }).on("error", (e) =>{
+      console.log(e);
+    })
+  })
+})
+
+app.post('/names', function(req, res){
+  var option = req.body.options;
+
+  if(option == "male"){
+    res.send("<h1> The male dog name is : "+ dogNames.maleRandom());
+  }
+  else{
+    res.send("<h1> The female dog name is :"+ dogNames.femaleRandom());
+  }
+
+
+})
+
+app.get('/villains', function(req, res){
+  res.send("<h1>" + villains.random());
+})
+app.post('/', function(req, res){
+  var fullname = req.body.personname;
+  res.write("<h1> Welcome  " + fullname);
+  res.write("<br> <h1> your saviour is " + heroes.random());
+  res.write("<br><br> Please stay away from " + villains.random());
+  res.send();
+});
+
+
+app.listen(port, function(req, res){
+  console.log("server is up and running on port" + port);
+});
