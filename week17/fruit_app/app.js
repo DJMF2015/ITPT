58c3f26e3d5749dc06027d000dcc91df428f5d7b
DJMF2015 <djm.fulton@gmail.com>
2020-06-17 09:51:12 +0100

    completion of exercise

--- week17/fruit_app/app.js
+++ week17/fruit_app/app.js
@@ -2,7 +2,7 @@
 // const { strictEqual } = require('assert');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/documentDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/fruitsDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const fruitSchema = new mongoose.Schema({
     name: String,
@@ -13,21 +13,56 @@ const fruitSchema = new mongoose.Schema({
 //new instance of fruitsSchema
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const fruit = new Fruit({
-    name: String,
-    rating: 7,
-    review: "pretty good"
+const orange = new Fruit({
+    name: "Orange",
+    rating: 88,
+    review: "Delicious"
 });
 
- //save to database;
- // fruit.save();
- // console.log(fruit)
+const kiwi = new Fruit({
+    name: "kiwi",
+    rating: 2,
+    review: "fantastic"
+});
 
+const banana = new Fruit({
+    name: "Banana",
+    rating: 10,
+    review: "Top Banana"
+});
+
+// Fruit.insertMany([orange, kiwi, banana], (function (err) {
+//     if (err) {
+//         console.log(err)
+//     }
+//     else {
+//         console.log("successfully inserted many")
+//     }
+// }));
+//save to database;
+//  fruit.save();
+//  console.log(fruit)
+Fruit.find(function (err, fruits) {
+    var fruitArray = [];
+    fruits.forEach(function(fruit){
+        fruitArray.push(fruit.name)
+        console.log(fruit.name)
+    if (err) {
+        console.log(err)
+     } else {
+      console.log("successful")
+            }
+        // console.log("Name of fruits: " + fruitArray)
+    });
+});
+
+//define schema
 const personSchema = new mongoose.Schema({
     name: String,
     age: Number
 });
 
+//define new instance of model
 const Person = mongoose.model("Person", personSchema);
 
 const person = new Person({
@@ -35,7 +70,7 @@ const person = new Person({
     age: 37
 
 });
-
+//save
 // person.save();
 // console.log(person);
 
@@ -51,11 +86,10 @@ const document = new Document({
     age: 25
 });
 
-document.save();
-console.log(document);
+// document.save();
+// console.log(document);
 
-
-
+//find
 const findDocuments = function (db, callback) {
     const collection = db.collection('fruits');
     //find all documents
